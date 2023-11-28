cc5d95c1f284e999b2ce0d444a2a8e4125a5881b
Amr GabAllah <amr.gaballah@rology.net>
2023-11-28 20:15:01 +0200

    fix unit test async calls

--- Services/DriverServiceTest.cs
+++ Services/DriverServiceTest.cs
@@ -11,73 +11,73 @@ namespace Services
     {
 
         [Fact]
-        public void AddDriver_ShouldCallRepositoryAddDriver()
+        public async Task AddDriver_ShouldCallRepositoryAddDriver()
         {
             var mockRepo = new Mock<IDriverRepository>();
             var service = new DriverService(mockRepo.Object);
             var driver = new Driver();
 
-            service.AddDriver(driver);
+            await service.AddDriver(driver);
 
             mockRepo.Verify(repo => repo.AddDriver(driver), Times.Once);
         }
 
         [Fact]
-        public void UpdateDriver_ShouldCallRepositoryUpdateDriver()
+        public async Task UpdateDriver_ShouldCallRepositoryUpdateDriver()
         {
             var mockRepo = new Mock<IDriverRepository>();
             var service = new DriverService(mockRepo.Object);
             var driver = new Driver();
 
-            service.UpdateDriver(driver);
+            await service.UpdateDriver(driver);
 
             mockRepo.Verify(repo => repo.UpdateDriver(driver), Times.Once);
         }
 
         [Fact]
-        public void DeleteDriver_ShouldCallRepositoryDeleteDriver()
+        public async Task DeleteDriver_ShouldCallRepositoryDeleteDriver()
         {
 
             var mockRepo = new Mock<IDriverRepository>();
             var service = new DriverService(mockRepo.Object);
             var driverId = 1;
 
-            service.DeleteDriver(driverId);
+            await service.DeleteDriver(driverId);
 
             mockRepo.Verify(repo => repo.DeleteDriver(It.Is<Driver>(d => d.Id == driverId)), Times.Once);
         }
 
         [Fact]
-        public void GetDrivers_ShouldCallRepositoryGetDrivers()
+        public async Task GetDrivers_ShouldCallRepositoryGetDrivers()
         {
 
             var mockRepo = new Mock<IDriverRepository>();
             var service = new DriverService(mockRepo.Object);
 
-            service.GetDrivers();
+            await service.GetDrivers();
 
             mockRepo.Verify(repo => repo.GetDrivers(), Times.Once);
         }
 
         [Fact]
-        public void GetAlphabetizedDrivers_ShouldCallRepositoryGetAlphabetizedDrivers()
+        public async Task GetAlphabetizedDrivers_ShouldCallRepositoryGetAlphabetizedDrivers()
         {
 
             var mockRepo = new Mock<IDriverRepository>();
             var service = new DriverService(mockRepo.Object);
 
-            service.GetAlphabetizedDrivers();
+            await service.GetAlphabetizedDrivers();
 
             mockRepo.Verify(repo => repo.GetAlphabetizedDrivers(), Times.Once);
         }
 
         [Fact]
-        public void Add10Randoms_ShouldCallAddDriverTenTimes()
+        public async Task Add10Randoms_ShouldCallAddDriverTenTimes()
         {
             var mockRepo = new Mock<IDriverRepository>();
             var service = new DriverService(mockRepo.Object);
 
-            service.Add10Randoms();
+            await service.Add10Randoms();
 
             mockRepo.Verify(repo => repo.AddDriver(It.IsAny<Driver>()), Times.Exactly(10));
         }
